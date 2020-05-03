8998da608f2d4395266da196773e417c5ff99368
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2020-05-03 17:41:29 +0200

    Bump minimum Leiningen version
    A while ago we configured Travis-CI integration to use Leiningen 2.9.
    But we didn't also bump the minimum verion in project.clj. So make
    sure we are in sync.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject hydrogen/duct-template "0.3.10-SNAPSHOT"
   :description "Hydrogen duct template"
+  :min-lein-version "2.9.0"
   :url "https://github.com/magnetcoop/hydrogen.duct-template"
   :license {:name "Mozilla Public License 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
