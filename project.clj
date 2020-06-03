26a7894ddda096e2870092a325655d5643ed36af
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2020-06-03 13:01:06 +0200

    Prepare for next development release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hydrogen/duct-template "0.4.1"
+(defproject hydrogen/duct-template "0.4.2-SNAPSHOT"
   :description "Hydrogen duct template"
   :min-lein-version "2.9.0"
   :url "https://github.com/magnetcoop/hydrogen.duct-template"
