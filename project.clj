e17f4e476fb6bdaf63371ec854f6601ea72fc006
Damian Hryniewicz <damian@magnet.coop>
2020-09-14 18:22:29 +0200

    Release v0.5.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hydrogen/duct-template "0.4.4-SNAPSHOT"
+(defproject hydrogen/duct-template "0.5.0"
   :description "Hydrogen duct template"
   :min-lein-version "2.9.0"
   :url "https://github.com/magnetcoop/hydrogen.duct-template"
