19e6d2fde490c06aef64bd45baba40b49c868dd8
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2020-12-03 13:45:05 +0100

    Fix missing routes and namespaces in Cognito sessions
    They were added when using Keycloak sessions, but not when using
    Cognito sessions.

--- src/hydrogen/session/cognito/duct_template.clj
+++ src/hydrogen/session/cognito/duct_template.clj
@@ -4,7 +4,7 @@
 
 (ns hydrogen.session.cognito.duct-template
   (:require [hydrogen.session.core :as core]
-            [hydrogen.utils :as utils :refer [resource ns->dir-name]]))
+            [hydrogen.utils :as utils :refer [resource ns->dir-name gen-cascading-routes]]))
 
 (def ^:private ^:const cognito-config
   ":cognito
@@ -13,12 +13,20 @@
 
 (defn- profile-base [project-ns]
   (let [api-config-kw (keyword (str project-ns ".api") "config")
-        api-config (format core/api-config-profile-base cognito-config)]
-    (assoc core/session-core-profile-base api-config-kw api-config)))
+        api-config (format core/api-config-profile-base cognito-config)
+        api-user-kw (keyword (str project-ns ".api") "user")
+        api-user core/api-user-profile-base]
+    (-> core/session-core-profile-base
+        (assoc api-config-kw api-config)
+        (assoc api-user-kw api-user))))
 
 (defn profile [{:keys [project-ns profiles]}]
   {:vars {:hydrogen-session? true
-          :hydrogen-session-cognito? true}
+          :hydrogen-session-cognito? true
+          :cascading-routes (gen-cascading-routes project-ns ["static/root"
+                                                              "api/config"
+                                                              "api/example"
+                                                              "api/user"])}
    :deps '[[duct/middleware.buddy "0.2.0"]
            [magnet/buddy-auth.jwt-oidc "0.10.0"]]
    :templates {;; Client
@@ -26,14 +34,16 @@
                "src/{{dirs}}/client/session.cljs" (resource "session/cognito/cljs/session.cljs")
                "src/{{dirs}}/client/user.cljs" (resource "session/user.cljs")
                "src/{{dirs}}/client/foreign-libs/externs/cognito.js" (resource "session/cognito/cljs/foreign-libs/externs/cognito.js")
+               ;;API
+               "src/{{dirs}}/api/user.clj" (resource "session/keycloak/api/user.clj")
                ;; Resources
                "resources/{{dirs}}/public/css/landing.scss" (resource "session/cognito/resources/css/landing.scss")
                "resources/{{dirs}}/public/images/email-address.svg" (resource "session/cognito/resources/images/email-address.svg")
                "resources/{{dirs}}/public/images/password.svg" (resource "session/cognito/resources/images/password.svg")}
    :profile-base (profile-base project-ns)
    :modules {:hydrogen.module/core (cond->
-                                    {:externs-paths
-                                     {:production
-                                      [(str (ns->dir-name project-ns) "/client/foreign-libs/externs/cognito.js")]}}
+                                       {:externs-paths
+                                        {:production
+                                         [(str (ns->dir-name project-ns) "/client/foreign-libs/externs/cognito.js")]}}
                                      (utils/use-figwheel-main? profiles)
                                      (assoc :figwheel-main true))}})
