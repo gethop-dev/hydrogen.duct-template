gethop-dev/hydrogen.duct-template

commit 08bcff44e5a0dce14c5521416a3ff02887b47c57

Fix fetch-user-data event handler
    This event handler now gets jwt-token from session cofx instead of appdb.
    It is so because at times the token may not be present in appdb yet when
    ::ensure-data is called.

resources/session/user.cljs
@@ -18,13 +18,22 @@
  (fn [db [_ user]]
    (assoc db :user user)))
 
+(defn- fetch-user-data-event-fx
+  "This event handler gets jwt-token from session cofx instead of appdb.
+  It is so because at times the token may not be present in appdb yet when
+  ::ensure-data is called."
+  [{:keys [session] :as cofx} _]
+  {:pre [(contains? cofx :session)
+         (s/valid? ::session/session-cofx-spec session)]}
+  {:http-xhrio {:headers {"Authorization" (str "Bearer " (:jwt-token session))}
+                :method :get
+                :uri "/api/user"
+                :format (ajax/json-request-format)
+                :response-format (ajax/json-response-format {:keywords? true})
+                :on-success [::set-user-data]
+                :on-failure [::util/generic-error]}})
+
 (rf/reg-event-fx
- ::fetch-user-data
- (fn [{:keys [db]} _]
-   {:http-xhrio {:headers {"Authorization" (str "Bearer " (:jwt-token db))}
-                 :method :get
-                 :uri "/api/user"
-                 :format (ajax/json-request-format)
-                 :response-format (ajax/json-response-format {:keywords? true})
-                 :on-success [::set-user-data]
-                 :on-failure [::util/generic-error]}}))
+  ::fetch-user-data
+  [(rf/inject-cofx :session)]
+  fetch-user-data-event-fx)

commit 750d6e5ef0046116d519119fbf8bcf479fffc8aa

Fix user session breakage
08bcff4 introduced a fix for fetch-user-data event handler that uses
function `valid?` from `clojure.spec.alpha` namespace and specification
`session-cofx-spec` from `<<namespace>>.user.session` namespace. But the
requirers for those two namespaces were not added. Thus compilation
fails for the `<<namespace>>.client.user` namespace and user session
doesn't work.

Adding the missing requires fixes the issue.

[Fix #36]

resources/session/user.cljs
@@ -5,7 +5,9 @@
 {{=<< >>=}}
 (ns <<namespace>>.client.user
   (:require [ajax.core :as ajax]
+            [clojure.spec.alpha :as s]
             [re-frame.core :as rf]
+            [<<namespace>>.client.session :as session]
             [<<namespace>>.client.util :as util]))
 
 (rf/reg-sub