c43b753d30daa94f2c7d4777b19b838fc6ab1fe8
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2022-04-13 01:00:33 +0200

    Bump project dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :url "https://github.com/magnetcoop/hydrogen.duct-template"
   :license {:name "Mozilla Public License 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.11.0"]]
   :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
                                       :username :env/clojars_username
                                       :password :env/clojars_password
@@ -17,5 +17,5 @@
   :profiles
   {:dev          [:project/dev :profiles/dev]
    :profiles/dev {}
-   :project/dev  {:plugins [[jonase/eastwood "0.3.11"]
-                            [lein-cljfmt "0.6.7"]]}})
+   :project/dev  {:plugins [[jonase/eastwood "1.2.3"]
+                            [lein-cljfmt "0.8.0"]]}})
